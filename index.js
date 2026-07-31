f9ec480f92247b7c960df25bc761770e117e3113
Breadknight <breadknight12@gmail.com>
2026-07-31 09:09:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let mason = ();
+let detected;
 mason = (true);
 if (mason) {
   console.log('MASON DETECTED');
+  detected = (true)
 } else {
   console.log('NO MASON DETECTED');
+  detected = (false)
+}
+
+function isMasonDetecte() {
+  if detected(){
+      return 54 + 13;
+  }
 }
